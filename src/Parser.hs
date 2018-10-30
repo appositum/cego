b25ee1d26a7e0eec39de67fc86160b9e9917c43c
eddie <appositum@protonmail.com>
2018-10-30 00:21:00 -0300

    Types

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,3 +2,20 @@ module Parser where
 
 import Control.Applicative
 import Text.Trifecta
+
+data NumberOrString = NOSS String | NOSI Integer
+  deriving (Eq, Show)
+          
+type Major = Integer
+type Minor = Integer                            
+type Patch = Integer                       
+type Release = [NumberOrString]              
+type Metadata = [NumberOrString]
+
+data SemVer = SemVer
+  { getMajor    :: Major
+  , getMinor    :: Minor
+  , getPatch    :: Patch
+  , getRelease  :: Release
+  , getMetadata :: Metadata
+  } deriving (Eq, Show)
