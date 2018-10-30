c72a5c29209b47a65cc861f495a47cffe44e1c16
eddie <appositum@protonmail.com>
2018-10-30 03:07:43 -0300

    Fix tests and add new metadata case

--- test/Spec.hs
+++ test/Spec.hs
@@ -30,19 +30,20 @@ main = hspec $ do
       case parse "1.0.0-alpha+001" of
         Failure err -> fail (show err)
         Success res ->
-          res `shouldBe` SemVer 1 0 0 [NOSS "alpha"] [NOSS "001"]
+          res `shouldBe` SemVer 1 0 0 [NOSS "alpha"] [NOSI 1]
 
     it "1.0.0+20130313144700" $ do
-      case parse "1.0.0-beta" of
+      case parsse "1.0.0+20130313144700" of
         Failure err -> fail (show err)
         Success res ->
-          res `shouldBe` SemVer 1 0 0 [NOSS "beta"] [NOSS "20130313144700"]
+          res `shouldBe` SemVer 1 0 0 [] [NOSI 20130313144700]
 
     it "1.0.0-beta+exp.sha.5114f85" $ do
-      case parse "1.0.0-beta" of
+      case parse "1.0.0-beta+exp.sha.5114f85" of
         Failure err -> fail (show err)
         Success res ->
-          res `shouldBe` SemVer 1 0 0 [NOSS "beta"] [NOSS "exp", NOSS "sha", NOSS "5114f85"]
+          res `shouldBe` SemVer 1 0 0 [NOSS "beta"]
+              [NOSS "exp", NOSS "sha", NOSS "5114f85"]
 
     it "1.0.0-alpha.1" $ do
       case parse "1.0.0-alpha.1" of
@@ -54,10 +55,17 @@ main = hspec $ do
       case parse "1.0.0-0.3.7" of
         Failure err -> fail (show err)
         Success res ->
-          res `shouldBe` SemVer 1 0 0 [NOSI 0, NOSI 3, NOSI 7 ] []
+          res `shouldBe` SemVer 1 0 0 [NOSI 0, NOSI 3, NOSI 7] []
 
     it "1.0.0-x.7.z.92" $ do
       case parse "1.0.0-x.7.z.92" of
         Failure err -> fail (show err)
         Success res ->
           res `shouldBe` SemVer 1 0 0 [NOSS "x", NOSI 7, NOSS "z", NOSI 92] []
+
+    it "6.6.6-alpha+exp.opa.0312ff03.1.2" $ do
+      case parse "6.6.6-alpha+exp.opa.0312ff03.1.2" of
+        Failure err -> fail (show err)
+        Success err ->
+          res `shouldBe` SemVer 6 6 6 [NOSS "alpha"]
+              [NOSS "exp", NOSS "opa", NOSS "0312ff03", NOSI 1, NOSI 2]
